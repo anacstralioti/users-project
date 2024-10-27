11166c831d51075bf81ca27bc60c770fad76529b
Ana Carolina <anastralioti@gmail.com>
2024-10-27 12:00:05 -0300

    first commit

---
+++ schema.sql
@@ -0,0 +1,9 @@
+create table if not exists usuarios (
+    id integer primary key autoincrement,
+    login varchar not null unique,
+    senha varchar not null,
+    nome varchar not null,
+    created timestamp default current_timestamp,
+    modified timestamp default current_timestamp,
+    status smallint default 1
+);
